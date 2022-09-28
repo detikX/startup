89c4058394c143c5fd8cd367750a6e6ec23f3a82
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-09-28 08:26:01 +0700

    update

--- js/app-backup-scrolltrigger.js
+++ js/app-backup-scrolltrigger.js
@@ -151,5 +151,53 @@ $.ajax({
       `)
     }
 
+          // Horizontal scroll
+      gsap.registerPlugin(ScrollTrigger)
+
+      const container = document.querySelector(".horizontal-container");
+
+      gsap.to(container, {
+        x: () => -((container.scrollWidth) - document.documentElement.clientWidth) + "px",
+        ease: "none",
+        scrollTrigger: {
+          trigger: container,
+          invalidateOnRefresh: true,
+          pin: true,
+          markers: true,
+          scrub: 1,
+          start:"top 20%",
+          // end: () => "+=" + ((container.offsetWidth - innerWidth) / 2)
+          end: () => "+=" + (((container.offsetWidth - innerWidth) ))
+        }
+      })
+
+      // console.log(container.scrollWidth);
   }
 })
+
+
+// Skew
+let proxy = { skew: 0 },
+    skewSetter = gsap.quickSetter("#skew", "skewY", "deg"),
+    clamp = gsap.utils.clamp(-20, 20); 
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / 300);
+    if (Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {
+        skew: 0,
+        duration: 0.8,
+        ease: "power3",
+        overwrite: true,
+        onUpdate: () => skewSetter(proxy.skew)});
+    }
+  }
+});
+
+gsap.set("#skew", {transformOrigin: "left center", force3D: true});
+
+
+
+
